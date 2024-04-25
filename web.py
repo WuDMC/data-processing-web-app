e14aa9323f2e05c38a9983d01af18227413fc32b
DENIS MIRONOV <65350779+WuDMC@users.noreply.github.com>
2024-04-25 23:30:23 +0000

    format

--- web.py
+++ web.py
@@ -4,11 +4,13 @@ import os
 import json
 
 app = Flask(__name__)
-face_detector = FaceDetector(json.loads(os.getenv("GCP_CREDS"))) 
+face_detector = FaceDetector(json.loads(os.getenv("GCP_CREDS")))
+
 
 @app.route("/")
 def index():
     return jsonify({"Hello": "Mitek"})
+
 
 @app.route("/detect_faces", methods=["POST"])
 def detect_faces():
@@ -25,10 +27,12 @@ def detect_faces():
         except Exception as e:
             return jsonify({"error": str(e)}), 500
 
+
 @app.route("/detect_faces", methods=["GET"])
 def curl_example():
-    curl_command = 'curl -X POST \\\n  http://127.0.0.1:8080/detect_faces \\\n  -H \'Content-Type: application/json\' \\\n  -d \'{"image_url": "YOUR_IMAGE_URL_HERE"}\''
+    curl_command = "curl -X POST \\\n  http://127.0.0.1:8080/detect_faces \\\n  -H 'Content-Type: application/json' \\\n  -d '{\"image_url\": \"YOUR_IMAGE_URL_HERE\"}'"
     return jsonify({"curl_example": curl_command})
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
