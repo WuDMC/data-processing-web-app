451adc16e67dbe8f58926ca3685f5dd67a54fa05
DENIS MIRONOV <65350779+WuDMC@users.noreply.github.com>
2024-04-28 00:15:56 +0000

    ffmpeg

--- audio.py
+++ audio.py
@@ -2,6 +2,7 @@ import requests
 import wave
 import base64
 import os
+import subprocess
 
 
 def download_file(url):
@@ -13,12 +14,25 @@ def download_file(url):
         return None
 
 
-def convert_to_wav(file_content, output_file):
-    with wave.open(output_file, "wb") as wf:
-        wf.setnchannels(2)  # Задаем количество каналов
-        wf.setsampwidth(2)  # Задаем размер сэмпла в байтах
-        wf.setframerate(16000)  # Задаем частоту дискретизации
-        wf.writeframes(file_content)
+def convert_to_wav(file_content, file_name, output_file):
+    # Создаем временный файл с исходным аудио контентом
+    temp_input_file = f"temp_input{file_name}"
+    with open(temp_input_file, "wb") as temp_file:
+        temp_file.write(file_content)
+
+    # Команда ffmpeg для конвертации файла в WAV с частотой дискретизации 16 кГц
+    ffmpeg_command = [
+        "ffmpeg",
+        "-i", temp_input_file,
+        "-ar", "16000",  # Частота дискретизации 16 кГц
+        output_file
+    ]
+
+    # Выполняем команду ffmpeg
+    subprocess.run(ffmpeg_command, check=True)
+
+    # Удаляем временный файл
+    os.remove(temp_input_file)
 
 
 def convert_to_base64(input_file):
@@ -38,23 +52,18 @@ def delete_file(file_path):
 def process_audio(url):
     file_content = download_file(url)
     if file_content:
-        # # Extract filename from the URL
-        # file_name = url.split("/")[-1]
-        # # Save the downloaded file
-        # with open(file_name, "wb") as f:
-        #     f.write(file_content)
-
-        # # Convert to WAV
-        # wav_file = "output.wav"
-        # convert_to_wav(file_content, wav_file)  # Pass file content instead of filename
-
-        # # Convert to Base64
-        # base64_string = convert_to_base64(wav_file)
+        file_name = url.split("/")[-1]
+        with open(file_name, "wb") as f:
+            f.write(file_content)
+            
+        wav_file = f"{file_name}.wav"
+        convert_to_wav(file_content, file_name, wav_file)  
+        # Convert to Base64
+        base64_string = convert_to_base64(wav_file)
         base64_string = base64.b64encode(file_content).decode("utf-8")
 
-        # Delete temporary files
-        # delete_file(file_name)
-        # delete_file(wav_file)
+        delete_file(file_name)
+        delete_file(wav_file)
 
         return base64_string
     else:
--- tests/test_audio.py
+++ tests/test_audio.py
@@ -4,17 +4,14 @@ from audio import process_audio
 
 @pytest.mark.parametrize(
     "url",
-    ["https://file-examples.com/wp-content/storage/2017/11/file_example_MP3_700KB.mp3"],
+    ["https://download.samplelib.com/mp3/sample-9s.mp3"],
 )
 def test_process_audio(url):
     base64_string = process_audio(url)
     assert base64_string is not None, "The process_audio function returned None"
     assert isinstance(base64_string, str), "The return value is not a string"
     assert len(base64_string) > 0, "The Base64 string received is empty"
-    # assert base64_string.startswith(
-    #     "UklGRu"
-    # ), "The Base64 string does not start with 'UklGRu'"
     assert base64_string.startswith(
-        "PCFE"
-    ), "The Base64 string does not start with 'PCFE'"
+        "SUQzBAAAAA"
+    ), "The Base64 string does not start with 'SUQzBAAAAA'"
 
